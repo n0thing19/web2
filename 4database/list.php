dae80f5a60de8bf9e6f15ec5937fa6400d1ce4d3
n0thing19 <62434029+n0thing19@users.noreply.github.com>
2025-06-09 19:59:47 +0700

    filter status and task

--- 4database/list.php
+++ 4database/list.php
@@ -5,12 +5,38 @@ use Uph\Database\DB;
 use Uph\Database\Twig;
 $twig = Twig::make('templates');
 $db = DB::getDB();
-$q = $db->query('SELECT * FROM todo');
+$sql = 'SELECT * FROM todo';
+//query builder
+$where = [];
+$params = [];
+
+if ($_GET['search_task'] ?? null) {
+    $where[] = "task LIKE :search_task";
+    $params['search_task'] = '%' . $_GET['search_task'] . '%';
+}
+
+if ($_GET['search_status'] ?? null) {
+    $where[] = "status = :search_status";
+    $params['search_status'] = $_GET['search_status'];
+
+}
+
+
+if (count($where) > 0) {
+    $sql .= ' WHERE ' . implode(' AND ', $where);
+}
+
+die($sql);
+
+$q = $db->prepare($sql);
+$q->execute($params);
 $rows = $q->fetchAll();
 echo $twig->render(
     'list.twig.html',
     [
         'rows' => $rows,
+        'search_task' => $_GET['search_task'] ?? null,
+        'search_status' => $_GET['search_status'] ?? null,
         'rows_count' => count($rows)
     ]
 );
